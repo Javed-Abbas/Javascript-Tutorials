cf0fd5003cd27165cfbd48bb84ac286cedc9be60
Javed Abbas <123265684+Javed-Abbas@users.noreply.github.com>
2023-09-17 11:55:25 +0000

    variables and data types

---
+++ Basics_Javascript/DataTypes.js
@@ -0,0 +1,16 @@
+"use strict"     //use only for newer version
+
+
+
+            // primitive data types
+let name = "javed abbas"          //string type
+let age = 23                      // number type
+let googleUserName = true/false  //bolean type
+let city;                        // undefined value
+let state = null;                //standalone value/object type
+                                 //bignit is a data type
+                                 //symbol is a data type                                
+
+
+                                // object
+console.log(name);
---
+++ Basics_Javascript/Variables.js
@@ -0,0 +1,8 @@
+const userId = 12121212;
+let userEmail = "javed@gmail.com"
+var userPassword = "212121"
+userCity = "khanpur"
+
+//userId = 1111;
+
+console.table([userId,userEmail,userPassword,userCity])
