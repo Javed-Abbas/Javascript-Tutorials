16103cff639f45ac006ebb8f850d4f77ba128ff1
Javed Abbas <123265684+Javed-Abbas@users.noreply.github.com>
2023-09-21 05:09:27 +0000

    DataTypesSummery.js

---
+++ Basics_Javascript/DataTypes.Summary.js
@@ -0,0 +1,47 @@
+// Youtube channel "Chaiaurcode" video no = 9
+
+// Premitive Data Types
+
+//String
+//Number
+//Boolean
+//null
+//Undefined
+//Symbol
+//BigInt
+
+const score = 34                // number example
+
+const isLoggedIn = false        // Boolean example
+
+const Id = Symbol("123")        //Two Symbol example
+const UserId = Symbol("123")    
+
+const userNum = 432463456       // BigInt Example
+
+const empty = null              //Null example
+
+//const UserEmai;                 // Undefined example
+
+
+
+//Reference  (Non-Premitive) Data-types
+
+//Array 
+//OBject
+//Function
+
+const heros = ["Allama Iqbal", "Quaid-e-Azam", "Sir syed ahmad khan"]        //Array example
+
+//console.log(heros);
+
+const myObj = {                                                            //Object Example
+    name: "Javed",
+    age: 19,
+    gender: "Male"
+}
+
+
+const myFunction = () => {                                               // Function example
+    console.log("Hello World");
+}
