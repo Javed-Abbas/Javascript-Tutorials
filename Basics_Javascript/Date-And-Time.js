6ef5764607031abb1dc6be1ac8731986de4e3e65
Javed Abbas <123265684+Javed-Abbas@users.noreply.github.com>
2023-10-02 07:52:07 +0000

    Date and Time incomplete now

---
+++ Basics_Javascript/Date-And-Time.js
@@ -0,0 +1,15 @@
+//Topic: Dates and Time               Yputube Channel: Chai aur code            Video No: !3
+
+const myDate = new Date()
+//console.log(myDate)
+//console.log(myDate.toString())
+//console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+//console.log(typeof myDate)
+
+const mySpaceficDate = new Date(2023, 4, 17)
+//console.log(mySpaceficDate.toDateString())
+//console.log(mySpaceficDate.toLocaleDateString())
+
+const mySpaceficTime = new Date(2023, 6, 7, 5, 10)
+console.log(mySpaceficTime.toLocaleString())
